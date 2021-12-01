9031edcb6fd60d6a39f6e8437549168e0a82ab72
Andy Hansen <arhansen@crimson.ua.edu>
2021-12-01 09:14:48 -0600

    First file with cmake files

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cmake -DCMAKE_BUILD_TYPE=Release .
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by andyh on 12/1/21.
+//
+
+#include <iostream>
+using namespace std;
+int main(void) { cout << "Hello" << endl; return 0; }
